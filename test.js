96c17906726da8edc65948ef9a4914406fa744cc
IndexXuan <indexxuan@gmail.com>
2021-05-26 16:14:06 +0800

    chore: remove useless file

--- test.js
+++
@@ -1,5 +0,0 @@
-const path = require('path')
-
-const ret = path.dirname('/Users/indexxuan/my-mpa-js-app/pages/index/index.html')
-const ret1 = path.basename('/Users/indexxuan/my-mpa-js-app/pages/index/index.html')
-console.log(ret, 'ret: ')
