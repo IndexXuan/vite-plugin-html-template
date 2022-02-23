d806a6371d17a4670bac71e3cad402cf9901877b
IndexXuan <indexxuan@gmail.com>
2022-02-23 14:02:55 +0800

    chore(release): v1.1.2

--- src/index.ts
+++ src/index.ts
@@ -103,7 +103,8 @@ export default function htmlTemplate(userOptions: UserOptions = {}): Plugin {
           : id.startsWith(PREFIX)
       ) {
         const idNoPrefix = id.slice(PREFIX.length)
-        const pageName = last(path.dirname(id).split(isWin32 ? '\\' : '/')) || ''
+        // resolveId checked isWin32 already
+        const pageName = last(path.dirname(id).split('/')) || ''
 
         const page = options.pages[pageName] || {}
         const templateOption = page.template
